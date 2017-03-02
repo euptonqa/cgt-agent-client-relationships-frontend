229ab48b9957913d1dda073a9ea4d4aa4366b328
DavidWoodward42 <david.woodward@digital.hmrc.gov.uk>
2017-03-02 12:09:30 +0000

    Removed the context route file stucture from the project.

--- app/controllers/HelloWorld.scala
+++ app/controllers/HelloWorld.scala
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.cgtagentclientrelationshipsfrontend.controllers
+package controllers
 
-import uk.gov.hmrc.play.frontend.controller.FrontendController
-import play.api.mvc._
-import scala.concurrent.Future
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
+import play.api.mvc._
+import uk.gov.hmrc.play.frontend.controller.FrontendController
 
+import scala.concurrent.Future
 
 object HelloWorld extends HelloWorld
 
 trait HelloWorld extends FrontendController {
-  val helloWorld = Action.async { implicit request =>
-		Future.successful(Ok(uk.gov.hmrc.cgtagentclientrelationshipsfrontend.views.html.helloworld.hello_world()))
+  val helloWorld: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(views.html.helloworld.hello_world()))
   }
 }
--- test/controllers/HelloWorldControllerSpec.scala
+++ test/controllers/HelloWorldControllerSpec.scala
@@ -14,19 +14,15 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.cgtagentclientrelationshipsfrontend.controllers
+package controllers
 
 import play.api.http.Status
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.api.http._
-import play.api.test.FakeRequest
-import play.api.test.Helpers._
-import uk.gov.hmrc.play.test.UnitSpec
-import uk.gov.hmrc.play.test.{WithFakeApplication, UnitSpec}
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 
 
-class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication{
+class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication {
 
   val fakeRequest = FakeRequest("GET", "/")
 
